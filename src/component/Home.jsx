69f879ea82fc66d646d2bf2ced5c12042f80b8ca
nurlan626 <nurlan.mehdiyev.resume@gmail.com>
2022-01-17 15:52:10 +0400

    first commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="hero">
       <div className="card bg-dark text-white border-0">
-        <img src="/assets/bg.jpg" className="card-img" alt="Background" height="550px" />
+        <img src="/assets/bg.jpg" className="card-img" alt="Background" height="auto" />
         <div className="card-img-overlay d-flex flex-column justify-content-center">
             <div className="container">
           <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
